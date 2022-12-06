cfaa9f9b6052aff473f78d6633784681bd6e3def
Dan Olsen <dan.olsen@impartner.com>
2022-12-06 16:49:30 -0700

    day 6 tidy up

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -2,66 +2,54 @@
 
 var input = File.ReadAllText("..\\..\\..\\..\\input\\Day6.txt", Encoding.UTF8);
 
-List<(string a, int i, int j)> testCases = new List<(string a, int i, int j)>()
+var testCases = new List<(string a, int i, int j)>
 {
 	("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7, 19),
-	("bvwbjplbgvbhsrlpgdmjqwftvncz",5,23),
-	("nppdvjthqldpwncqszvftbrmjlhg", 6,23),
-	("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",10,29),
-	("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",11,26),
+	("bvwbjplbgvbhsrlpgdmjqwftvncz", 5, 23),
+	("nppdvjthqldpwncqszvftbrmjlhg", 6, 23),
+	("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10, 29),
+	("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, 26)
 };
 
 int StartOfPacketIndex(string message)
 {
 	var checkBuffer = new HashSet<char>();
-	List<char> lastFour = new List<char>(message.Take(3));
-	
-	for (int i = 3; i < message.Length; i++)
+	var lastFour = new List<char>(message.Take(3));
+
+	for (var i = 3; i < message.Length; i++)
 	{
 		lastFour.Add(message[i]);
 
-		if (lastFour.All(checkBuffer.Add))
-		{
-			return i+1;
-		}
+		if (lastFour.All(checkBuffer.Add)) return i + 1;
 		checkBuffer.Clear();
 		lastFour.RemoveAt(0);
-		
 	}
+
 	return message.Length;
 }
 
 foreach ((string a, int i, int ) testCase in testCases)
-{
 	Console.WriteLine($"{StartOfPacketIndex(testCase.a)} =? {testCase.i}");
-}
 
 Console.WriteLine($"Part1: {StartOfPacketIndex(input)}");
 
 int StartOfMessageIndex(string message)
 {
 	var checkBuffer = new HashSet<char>();
-	List<char> lastFour = new List<char>(message.Take(13));
+	var lastFour = new List<char>(message.Take(13));
 
-	for (int i = 13; i < message.Length; i++)
+	for (var i = 13; i < message.Length; i++)
 	{
 		lastFour.Add(message[i]);
 
-		if (lastFour.All(checkBuffer.Add))
-		{
-			return i + 1;
-		}
+		if (lastFour.All(checkBuffer.Add)) return i + 1;
 		checkBuffer.Clear();
 		lastFour.RemoveAt(0);
-
 	}
+
 	return message.Length;
 }
 
-foreach ((string a, int i, int j) testCase in testCases)
-{
-	Console.WriteLine($"{StartOfMessageIndex(testCase.a)} =? {testCase.j}");
-}
+foreach (var testCase in testCases) Console.WriteLine($"{StartOfMessageIndex(testCase.a)} =? {testCase.j}");
 
 Console.WriteLine($"Part1: {StartOfMessageIndex(input)}");
-
