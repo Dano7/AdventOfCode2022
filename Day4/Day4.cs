d3ecdd4e6d02356ec2d79cfa22e42fe7aa420d1c
Dan Olsen <dan.olsen@impartner.com>
2022-12-06 14:38:40 -0700

    Day 5 completed

--- Day4/Day4.cs
+++ Day4/Day4.cs
@@ -3,57 +3,45 @@ using System.Text;
 
 var input = File.ReadAllText("..\\..\\..\\..\\input\\Day4.txt", Encoding.UTF8);
 
-var assignmentPairs = input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
-
-Console.WriteLine($"true? {IsOneRangeFullyContainedByOther((new Range(1,3), new Range(2,3)))}");
-Console.WriteLine($"true? {IsOneRangeFullyContainedByOther((new Range(2,3), new Range(1,4)))}");
-
-
-
-
 (Range a, Range b)[] GetRanges(string[] strings)
 {
-  return strings.Select(s =>
-  {
-	  var pairs = s.Split(',');
-	  var ranges = pairs.Select(s1 =>
-	  {
-		  var range = s1.Split('-');
-		  return new Range(Convert.ToInt32(range[0]), Convert.ToInt32(range[1]));
-	  }).ToArray();
+	return strings.Select(s =>
+	{
+		var pairs = s.Split(',');
+		var ranges = pairs.Select(s1 =>
+		{
+			var range = s1.Split('-');
+			return new Range(Convert.ToInt32(range[0]), Convert.ToInt32(range[1]));
+		}).ToArray();
 
-	  return (a: ranges[0], b: ranges[1]);
-
-  }).ToArray();
+		return (a: ranges[0], b: ranges[1]);
+	}).ToArray();
 }
-
-
 
 static bool IsOneRangeFullyContainedByOther((Range a, Range b) ranges)
 {
-	int start1 = ranges.a.Start.Value;
-	int start2 = ranges.b.Start.Value;
-	int end1 = ranges.a.End.Value;
-	int end2 = ranges.b.End.Value;
+	var start1 = ranges.a.Start.Value;
+	var start2 = ranges.b.Start.Value;
+	var end1 = ranges.a.End.Value;
+	var end2 = ranges.b.End.Value;
 
-	var bContainsA = (start1 >= start2
-			 && start1 <= end2
-			 && end1 <= end2);
-	var AContainsB = (start2 >= start1
-			 && start2 <= end1
-			 && end1 >= end2);
-	return (bContainsA
-			|| AContainsB);
-
+	var bContainsA = start1 >= start2
+					 && start1 <= end2
+					 && end1 <= end2;
+	var AContainsB = start2 >= start1
+					 && start2 <= end1
+					 && end1 >= end2;
+	return bContainsA
+		   || AContainsB;
 }
 
 static bool IsAnyOverlapInRanges((Range a, Range b) ranges)
 {
-	int start1 = ranges.a.Start.Value;
-	int start2 = ranges.b.Start.Value;
-	int end1 = ranges.a.End.Value;
-	int end2 = ranges.b.End.Value;
-	
+	var start1 = ranges.a.Start.Value;
+	var start2 = ranges.b.Start.Value;
+	var end1 = ranges.a.End.Value;
+	var end2 = ranges.b.End.Value;
+
 	var Overlap = start1 <= end2 && end1 >= start2;
 
 	var DontOverlap = end1 < start2 || start1 > end2;
@@ -61,14 +49,9 @@ static bool IsAnyOverlapInRanges((Range a, Range b) ranges)
 	return Overlap;
 }
 
-
-
-var Ranges = GetRanges(assignmentPairs); 
-var x = Ranges.Where(IsOneRangeFullyContainedByOther).Count();
-
-var y = Ranges.Where(IsAnyOverlapInRanges).Count();
-Console.WriteLine($"Part1 count: {x}");
-Console.WriteLine($"Part2 count: {y}");
-
-
-//Console.WriteLine("Hello, World!");
+var assignmentPairs = input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+var Ranges = GetRanges(assignmentPairs);
+var Count1 = Ranges.Count(IsOneRangeFullyContainedByOther);
+var Count2 = Ranges.Count(IsAnyOverlapInRanges);
+Console.WriteLine($"Part1 count: {Count1}");
+Console.WriteLine($"Part2 count: {Count2}");
---
+++ Day5/Day5.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+var input = File.ReadAllText("..\\..\\..\\..\\input\\Day5.txt", Encoding.UTF8);
+var lines = input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+var stacksLine = lines.First(s => !s.Contains('['));
+var stacknums = stacksLine.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+var stackLines = lines.Take(Array.IndexOf(lines, stacksLine)).Reverse();
+var stacks = stacknums.ToDictionary(Convert.ToInt32, stacknum => new Stack<char>());
+
+BuildStacks(stackLines, stacks);
+
+void BuildStacks(IEnumerable<string> enumerable, Dictionary<int, Stack<char>> dictionary)
+{
+	foreach (var line in enumerable)
+		for (var i = 1; i < line.Length; i += 4)
+		{
+			var stacknum = (i + 3) / 4;
+			var crate = line[i];
+			if (crate != ' ') dictionary[stacknum].Push(crate);
+		}
+}
+
+var instructionLines = lines.Skip(Array.IndexOf(lines, stacksLine) + 1).ToArray();
+var moves = instructionLines.Select(s =>
+{
+	var x = s.Split(' ').Where((s, i) => i % 2 == 1).ToArray();
+	return (count: Convert.ToInt32(x[0]), from: Convert.ToInt32(x[1]), to: Convert.ToInt32(x[2]));
+}).ToArray();
+
+ExecuteMovesPart2(stacks, moves);
+
+void ExecuteMoves(Dictionary<int, Stack<char>> s, (int count, int from, int to)[] m)
+{
+	foreach (var move in m)
+		for (var i = 0; i < move.count; i++)
+			s[move.to].Push(s[move.from].Pop());
+}
+
+void ExecuteMovesPart2(Dictionary<int, Stack<char>> s, (int count, int from, int to)[] m)
+{
+	foreach (var move in m)
+	{
+		var bundle = string.Empty;
+		for (var i = 0; i < move.count; i++) 
+			bundle = string.Concat(s[move.from].Pop(), bundle);
+		foreach (var crate in bundle) 
+			s[move.to].Push(crate);
+	}
+}
+
+var message = stacks.Aggregate(string.Empty, (current, stack) => string.Concat(current, stack.Value.Peek()));
+
+Console.WriteLine($"Message: {message}");
